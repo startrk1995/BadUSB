27a5255a43cf57f2ade0dc7bb1de25714ad50475
Nathan Glaser <nate@wolf-paw.net>
2022-05-02 23:33:47 -0400

    switch pos

--- Bashbunny/MacOSX/RickRoll_MobyDick/setup.sh
+++ Bashbunny/MacOSX/RickRoll_MobyDick/setup.sh
@@ -1,5 +1,5 @@
 mkdir $HOME/Rickroll
-bash -c "cp /Volumes/BashBunny/payloads/switch2/* $HOME/Rickroll"
+bash -c "cp /Volumes/BashBunny/payloads/$Switch/* $HOME/Rickroll"
 bash -c "sed -i -e \"s/blah/$USER/g\" $HOME/Rickroll/com.youtube.rickroll.prank.plist"
 bash -c "cp $HOME/Rickroll/com.youtube.rickroll.prank.plist $HOME/Library/LaunchAgents/com.youtube.rickroll.prank.plist"
 bash -c "chmod 644 $HOME/Library/LaunchAgents/com.youtube.rickroll.prank.plist"
